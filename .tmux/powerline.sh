4ffd90563d5493dfe60a85958f16466c0daf7a40
Spencer Dixon <spencerlloyddixon@gmail.com>
2016-05-17 21:06:18 +0100

    Changed tmux to match new hybrid theme

--- .tmux/powerline.sh
+++ .tmux/powerline.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-LSEP=⮁
-RSEP=
+#LSEP=⮁
+#RSEP=
 
 # Monokai theme
-spotify_colour='#1ABD53'
-date_time_colour='colour55'
-spotify_text_colour='#eeeeee'
-date_time_text_colour='#eeeeee'
-black='colour236'
+#spotify_colour='#1ABD53'
+#date_time_colour='colour55'
+#spotify_text_colour='#eeeeee'
+#date_time_text_colour='#eeeeee'
+#black='colour236'
 
 # Base16-Eighties Theme
 #date_time_colour='colour04'
@@ -24,10 +24,17 @@ black='colour236'
 #spotify_colour="colour06"
 #black="colour00"
 
+# Hybrid theme
+date_time_colour=black
+spotify_text_colour='colour15'
+date_time_text_colour=white
+spotify_colour=cyan
+black=black
+
 SPOTIFY="#[fg=${spotify_colour},bg=${black},nobold,noitalics,nounderscore]$RSEP#[fg=${spotify_text_colour},bg=${spotify_colour},nobold,noitalics,nounderscore] $(osascript ~/.tmux/current_track.scpt)"
 
 DATE="#[fg=${date_time_colour},bg=${spotify_colour},nobold,noitalics,nounderscore] $RSEP#[fg=${date_time_text_colour},bg=${date_time_colour},nobold,noitalics,nounderscore] $(date +'%D')"
 
-TIME="#[fg=${date_time_text_colour},bg=${date_time_colour},nobold,noitalics,nounderscore]|#[fg=${date_time_text_colour},bg=${date_time_colour},nobold,noitalics,nounderscore] $(date +'%H:%M')"
+TIME="#[fg=${date_time_text_colour},bg=${date_time_colour},nobold,noitalics,nounderscore]#[fg=${date_time_text_colour},bg=${date_time_colour},nobold,noitalics,nounderscore] $(date +'%H:%M')"
 
-echo "$SPOTIFY $DATE $TIME" | sed 's/ *$/ /g'
+echo "$SPOTIFY" "$DATE | Hustle Mode: ON | $TIME" | sed 's/ *$/ /g'
